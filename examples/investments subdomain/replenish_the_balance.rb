0b663dc6829574364411adecb3c3ddb4f4084ca3
valexl <17valexl@gmail.com>
2021-03-13 13:28:01 +0300

    Update replenish the balance sample and UML

--- examples/investments subdomain/replenish_the_balance.rb
+++ examples/investments subdomain/replenish_the_balance.rb
@@ -1,31 +1,58 @@
 class Money
-  attr_reader :value, :currency
-  def initialize(value:, currency:)
+  attr_accessor :value
+  attr_reader :currency
+  
+  def initialize(value:, currency: "USD")
     @value = value
     @currency = currency
   end
 end
 
+class Cash
+  attr_reader :money
+  def initialize(money=nil)
+    @money = money || Money.new(value: 0)
+  end
+
+  def value
+    money.value
+  end
+
+  def add(money)
+    @money = Money.new(@money.value += money.value, currency: @money.currency)
+  end
+end
+
 class Balance
-  @instance = new
+  class << self
+    def replenish(money)
+      instance.replenish(money)
+    end
+
+    def instance
+      @instance
+    end
+  end
+  
+  attr_reader :cash
+  def initialize(cash:)
+    @cash = cash
+  end
+
+  @instance = new(cash: Cash.new)
   private_class_method :new
 
-  def self.replenish(money)
-    instance.replenish(money)
-  end
-
-  def self.instance
-    @instance
-  end
-
   def replenish(money)
-    puts "value - #{money.value}, currency - #{money.currency}"
+    cash.add(money)
+    puts "value - #{cash.value}"
     # do some logic
   end
 end
 
 
 money = Money.new(value: 100, currency: "RUB")
+balance = Balance.replenish(money)
+balance = Balance.replenish(money)
 balance = Balance.replenish(money)
 
 
