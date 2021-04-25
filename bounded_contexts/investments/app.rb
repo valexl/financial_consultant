6edfbd32f905fe43c5512e13c3ab59f4ac22a503
valexl <17valexl@gmail.com>
2021-04-25 08:55:08 +0300

    Add all get requests with empty views

--- bounded_contexts/investments/app.rb
+++ bounded_contexts/investments/app.rb
@@ -13,13 +13,43 @@ module FinancialConsultant
       use Rack::CommonLogger, Logger.new($stdout)
 
       plugin :all_verbs
-      plugin :render, escape: true
+      plugin :render, escape: true, views: "templates", layout: './layout'
       plugin :render_each
       plugin :partials
+      plugin :view_options
 
       route do |r|
         r.root do
-          view 'home'
+          r.redirect "/balance"
+        end
+
+        r.on "balance" do
+          set_view_subdir "balance"
+          r.root do
+            view "show"
+          end
+
+          r.get "replenish" do
+            view "replenish"
+          end
+        end
+
+        r.on "investments" do
+          set_view_subdir "investments"
+
+          r.get "open" do
+            view "open"
+          end
+
+          r.on Integer do |investment_id|
+            r.get "edit" do
+              view "edit"
+            end
+
+            r.get "close" do
+              view "close"
+            end
+          end
         end
       end
     end
---
+++ bounded_contexts/investments/spec/app_spec.rb
@@ -0,0 +1,41 @@
+require "spec_helper"
+
+RSpec.describe FinancialConsultant::Investments::App, roda: :app do
+  describe '/' do
+    before { get '/' }
+
+    it do
+      expect(last_response).to be_redirect
+      follow_redirect!
+      expect(last_request.path).to eql '/balance'
+    end
+  end
+
+  describe "balance/replenish" do
+    before { get 'balance/replenish' }
+    
+    it { is_expected.to be_successful }
+  end
+
+  context "investments" do
+    describe '/investments/open' do
+      before { get '/investments/open' }
+
+      it { is_expected.to be_successful }
+    end
+
+    describe '/investments/:investment_id/edit' do
+      let(:investment_id)  { 1 }
+      before { get "/investments/#{investment_id}/edit" }
+
+      it { is_expected.to be_successful }
+    end
+
+    describe '/investments/:investment_id/close' do
+      let(:investment_id)  { 1 }
+      before { get "/investments/#{investment_id}/close" }
+
+      it { is_expected.to be_successful }
+    end
+  end
+end
