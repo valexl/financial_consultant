99ebc94bc25c6cced15ded2bd1f28e126241e84f
valexl <17valexl@gmail.com>
2021-05-22 08:10:47 +0300

    Update cucmber test to use db

---
+++ bounded_contexts/investments/features/step_definitions/withdraw_money_defs.rb
@@ -0,0 +1,44 @@
+Before('@WithdrawMoney') do
+  cash = Cash.new(
+    rub_money: $money_creator.build_rub(value: 100_000),
+    usd_money: $money_creator.build_usd(value: 100_000),
+    eur_money: $money_creator.build_eur(value: 100_000)
+  )
+  balance = Balance.new(cash: cash, investments: [])
+  balance.investments = []
+  @investment_name = "cucumber_test"
+  
+  Repositories::BalanceRepository.create(balance)
+end
+
+When('investment opening by price {float} {string}') do |investment_price, currency|
+  @investment_details = OpenStruct.new(name: @investment_name, currency: currency, value: investment_price)
+  $browser.post '/api/investments/open', {
+    investment: { 
+      type: "apartment", 
+      name: @investment_details.name, 
+      price: { 
+        currency: @investment_details.currency, 
+        value: @investment_details.value
+      } 
+    }
+  }
+
+end
+
+When('investment closing with {float} {string} profit') do |profit, currency|
+  new_price = $money_creator.build(currency: @investment_details.currency, value: @investment_details.value + profit)
+
+  # TODO: add change price api endpoint
+  balance = Repositories::BalanceRepository.fetch
+  investment = balance.find_investment(name: @investment_details.name)
+  investment.change_price(new_price)
+  Repositories::BalanceRepository.save(balance)
+
+  $browser.post '/api/investments/close', { investment: { name: @investment_name } }
+end
+
+Then('{float} RUB is withdrawable') do |expected_withdrawable_money_rub|
+  balance = Repositories::BalanceRepository.fetch
+  expect(balance.withdrawable_money_rub.value).to eq(expected_withdrawable_money_rub)
+end
