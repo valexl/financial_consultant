d9631267cb51b704ef5a3bfdebb775d5fa1c92c7
valexl <17valexl@gmail.com>
2021-08-07 07:58:53 +0300

    make spec/integrations/investments_dividends_logic_spec.rb green

--- bounded_contexts/investments/spec/integrations/investments_dividends_logic_spec.rb
+++ bounded_contexts/investments/spec/integrations/investments_dividends_logic_spec.rb
@@ -4,11 +4,22 @@ RSpec.describe "Investments costs logic" do
   let(:money_creator) { MoneyCreator.new }
   let(:cash) do
     cash = Cash.new(
-      rub_money: money_creator.build_rub(initial_value: 0),
-      usd_money: money_creator.build_usd(initial_value: 20000, income: 1000, income_of_income: 500, income_of_income_of_income: 100),
-      eur_money: money_creator.build_eur(initial_value: 0)
+      rub_money: money_creator.build_rub(value: 0),
+      usd_money: money_creator.build_usd(
+        value: usd_value, 
+        initial_value_in_percent: usd_initial_value.to_f/usd_value, 
+        income_in_percent: usd_income.to_f/usd_value, 
+        income_of_income_in_percent: usd_income_of_income.to_f/usd_value, 
+        income_of_income_of_income_in_percent: usd_income_of_income_of_income.to_f/usd_value
+      ),
+      eur_money: money_creator.build_eur(value: 0)
     )
   end
+  let(:usd_value) { usd_initial_value + usd_income + usd_income_of_income + usd_income_of_income_of_income}
+  let(:usd_initial_value) { 20000 }
+  let(:usd_income) { 1000 }
+  let(:usd_income_of_income) { 500 }
+  let(:usd_income_of_income_of_income) { 100 }
   let(:balance) do
     Balance.new(cash: cash, investments: [])
   end
@@ -30,11 +41,11 @@ RSpec.describe "Investments costs logic" do
       end
 
       it "changes amount of invested money by dividend value (return some part of invested money)" do
-        expect(investment.invested_money.value).to eq(price.value - dividend.value)
+        expect(investment.invested_money.value.round).to eq(price.value - dividend.value)
       end
 
       it "increases amount of cash by dividend value" do
-        expect(cash.money(Currency::USD).value).to eq(11600 + 1000)
+        expect(cash.money(Currency::USD).value.round).to eq(11600 + 1000)
       end
 
       context "and there is price changing that covers all dividend earnings" do
@@ -50,16 +61,16 @@ RSpec.describe "Investments costs logic" do
           end
 
           it "closes investment with zero profit and loss" do
-            expect(cash.money(Currency::USD).value).to eq(21600)
+            expect(cash.money(Currency::USD).value.round).to eq(21600)
           end
 
           it "doesn't change income values" do
             usd_money = cash.money(Currency::USD)
-            expect(usd_money.value).to eq(21600)
-            expect(usd_money.initial_value).to eq(20000)
-            expect(usd_money.income).to eq(1000)
-            expect(usd_money.income_of_income).to eq(500)
-            expect(usd_money.income_of_income_of_income).to eq(100)
+            expect(usd_money.value.round).to eq(21600)
+            expect(usd_money.initial_value.round).to eq(20000)
+            expect(usd_money.income.round).to eq(1000)
+            expect(usd_money.income_of_income.round).to eq(500)
+            expect(usd_money.income_of_income_of_income.round).to eq(100)
           end
         end
       end
@@ -77,16 +88,16 @@ RSpec.describe "Investments costs logic" do
           end
 
           it "closes investment with profit" do
-            expect(cash.money(Currency::USD).value).to eq(31600)
+            expect(cash.money(Currency::USD).value.round).to eq(31600)
           end
 
           it "changes income values" do
             usd_money = cash.money(Currency::USD)
-            expect(usd_money.value).to eq(31600)
-            expect(usd_money.initial_value).to eq(20000)
-            expect(usd_money.income).to eq(1000 + 9259.2593)
+            expect(usd_money.value.round).to eq(31600)
+            expect(usd_money.initial_value.round).to eq(20000)
+            expect(usd_money.income).to eq(1000 + 9259.2589)
             expect(usd_money.income_of_income).to eq(500 + 462.963)
-            expect(usd_money.income_of_income_of_income).to eq(100 + 277.7777)
+            expect(usd_money.income_of_income_of_income).to eq(100 + 277.7779)
           end
         end
       end      
