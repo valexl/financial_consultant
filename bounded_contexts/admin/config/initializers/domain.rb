5e35556db9c812a5c541684f8161e6fbef3cfceb
Alexey V <17valexl@gmail.com>
2022-06-05 07:43:20 +0300

    Add DomainRegistry as ServiceFactory and add empty StartMonthService

--- bounded_contexts/admin/config/initializers/domain.rb
+++ bounded_contexts/admin/config/initializers/domain.rb
@@ -1,1 +1,3 @@
 require_relative '../../domain/model/month/month_repository_interface'
+require_relative '../../domain/model/domain_registry'
+require_relative '../../domain/model/month/start_month_service'
---
+++ bounded_contexts/admin/domain/model/month/start_month_service.rb
@@ -0,0 +1,10 @@
+module Admin
+  module Domain
+    module Model
+      module Month
+        class StartMonthService
+        end
+      end
+    end
+  end
+end
