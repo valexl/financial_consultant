dabb9145df56e40392f37bc0fbed5e6742457ce8
Alexey V <17valexl@gmail.com>
2022-06-05 08:31:00 +0300

    Define domain_event_publisher class

--- bounded_contexts/admin/config/initializers/domain.rb
+++ bounded_contexts/admin/config/initializers/domain.rb
@@ -5,3 +5,4 @@ require_relative '../../domain/model/month/month_repository_interface'
 require_relative '../../domain/model/month/start_month_service'
 
 require_relative '../../domain/model/domain_registry'
+require_relative '../../domain/model/domain_event_publisher'
