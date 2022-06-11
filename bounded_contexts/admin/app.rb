2b1fecde036c6e0d75660b3de4c1bf5090b39668
Alexey V <17valexl@gmail.com>
2022-06-11 15:37:24 +0300

    Add BUNDLE_GEMFILE ENV in app.rb to make karafka working

--- bounded_contexts/admin/app.rb
+++ bounded_contexts/admin/app.rb
@@ -5,5 +5,7 @@ require_relative '../common/common'
 ENV['RACK_ENV'] ||= 'development'
 Dotenv.load(".env.#{ENV['RACK_ENV']}")
 
+ENV["BUNDLE_GEMFILE"] = Dir.pwd + "/Gemfile"
+
 Dir[File.dirname(__FILE__) + '/config/initializers/*.rb'].each {|file| require file }
 
